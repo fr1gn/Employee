89a16a367d93c7e49706b4d1a2e9697f2be66055
Tairlan <231151@astanait.edu.kz>
2024-01-23 00:09:56 +0300

    oo

--- src/xrp/Student.java
+++ src/xrp/Student.java
@@ -1,7 +1,48 @@
 package xrp;
 
 public class Student {
+    private int id;
+    private static int id_gen;
+    private String name;
+    private String surname;
+    private double gpa;
+    Student() {
+        id = id_gen++;
+    }
+    Student(String name, String surname, double gpa) {
+        this();
+        setName(name);
+        setSurname(surname);
+        setGpa(gpa);
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
 
 
